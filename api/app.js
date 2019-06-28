3b09d2e0c72fc305ab71d52a3a6e7521cb1ddbb5
TigerZH <only_tigerhu@163.com>
2019-06-28 14:26:37 +0800

    await

--- api/app.js
+++ api/app.js
@@ -82,7 +82,7 @@ fs.readdirSync(path.join(__dirname, 'module')).reverse().forEach(file => {
     })
 })
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3002
 
 app.server = app.listen(port, () => {
     console.log(`server running @ http://localhost:${port}`)
