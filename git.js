68d97d4a45fd2df6d5d317d4473fb989ee9c7557
TigerZH <only_tigerhu@163.com>
2019-07-11 10:47:39 +0800

    await

--- git.js
+++ git.js
@@ -24,11 +24,12 @@ exe('git push');
 CFonts.say('TigerZH!', {
   font: 'block', // define the font face
   align: 'left', // define text alignment
-  colors: ['#fff'], // define all colors
+  colors: ['#fff','#ff8344'], // define all colors
   background: 'transparent', // define the background color, you can also use `backgroundColor` here as key
   letterSpacing: 1, // define letter spacing
   lineHeight: 1, // define the line height
   space: true, // define if the output text should have empty lines on top and on the bottom
   maxLength: '0', // define how many character can be on one line
 });
+
 exe('say all done!');
