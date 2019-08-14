d16d43366a7bc5aaea97f359fa8efe937e251837
TigerZH <only_tigerhu@163.com>
2019-08-14 15:55:38 +0800

    await

--- git.js
+++ git.js
@@ -7,7 +7,7 @@ const CFonts = require('cfonts');
 // eslint-disable-next-line consistent-return
 const exe = (cmd) => {
   let result;
-  
+
   try {
     result = shell.exec(cmd);
     if (result.code) {
@@ -22,15 +22,21 @@ exe('say 开始提交git!');
 exe('git add .');
 exe("git commit -m 'await' ");
 exe('git push');
-CFonts.say('TigerZH!', {
+var res = CFonts.say('TigerZH!', {
   font: 'block', // define the font face
   align: 'left', // define text alignment
-  colors: ['#fff','#ff8344'], // define all colors
+  colors: ['#fff', '#ff8344'], // define all colors
   background: 'transparent', // define the background color, you can also use `backgroundColor` here as key
   letterSpacing: 1, // define letter spacing
   lineHeight: 1, // define the line height
   space: true, // define if the output text should have empty lines on top and on the bottom
   maxLength: '0', // define how many character can be on one line
 });
+var done = "";
+console.log(res);
 
-exe('say all done!');
+if (res.code) {
+  done = res
+
+}
+exe(done);
