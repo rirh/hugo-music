bb6e76b1d2f16bef74fc616eb53363d25dbdb26a
zh <only_tigerhu@163.com>
2020-01-08 18:46:33 +0800

    await

--- src/stores/music.ts
+++ src/stores/music.ts
@@ -125,6 +125,7 @@ class Sound {
   private filter: any;
   private url: any;
   private vloume: number | string = 1;
+  private effectTimer: any;
   constructor(props: any = {}) {
     window.AudioContext = window.AudioContext || (window as any).webkitAudioContext;
     this.url = props.url;
@@ -196,7 +197,204 @@ class Sound {
       this.audio.pause();
     }, this.FADE_SEC * 800); // 延时
   }
+  public disconnect() {
+    this.gain.disconnect(0);
+    this.analyser.disconnect(0);
+  }
+  // 设置音效
+  public createSound() {
+    /*
+     * 标准 la 音为440hz， 高8音的频率为原音频的2倍
+     * 音阶换算公式: n个半音 = 12 * log2(f1 / f2) (其中, f1 < f2时n < 0, 反之n > 0);
+     * ->已知f2, 求f1, 其中 f1 小 f2 n个半音
+     * ->则 f1 = f2 * Math.pow(2, -n / 12);
+     */
+    const i = Math.ceil(Math.random() * 10);
+    const hz = 440;
+
+    // create Oscillator node
+    const oscillator = this.context.createOscillator();
+
+    oscillator.frequency.value = hz * i; // value in hertz
+    oscillator.connect(this.context.destination);
+    oscillator.start();
+    setTimeout(() => {
+      oscillator.stop();
+    }, 300);
+  }
+
+  public stereo(r: any) {
+    this.disconnect();
+    const panner = this.context.createPanner();
+    const gain1 = this.context.createGain();
+
+    let index = 0;
+    const radius = r || 20;
+    panner.setOrientation(0, 0, 0, 0, 1, 0);
+    // 让声源绕着收听者以20的半径旋转
+    this.effectTimer = setInterval(() => {
+      panner.setPosition(Math.sin(index) * radius, 0, Math.cos(index) * radius);
+      index += 1 / 100;
+    }, 16);
+    gain1.gain.value = 1;
+    this.gain.connect(gain1);
+    this.gain.connect(panner);
+    panner.connect(this.analyser);
+    this.analyser.connect(this.context.destination);
+  }
+
+  /*
+ * 低通滤波器
+ */
+  public lowpassFilter(freq: any) {
+    this.disconnect();
+    const biquadFilter = this.context.createBiquadFilter();
+    biquadFilter.type = 'lowpass'; // 低阶通过
+    biquadFilter.Q.value = 2;
+    biquadFilter.frequency.value = freq || 800; // 临界点的 Hz，默认800Hz
+    this.gain.connect(biquadFilter);
+    biquadFilter.connect(this.analyser);
+    this.analyser.connect(this.context.destination);
+  }
+  /*
+   * 高通滤波器
+   */
+  public highpassFilter(freq: any) {
+    this.disconnect();
+    const biquadFilter = this.context.createBiquadFilter();
+    biquadFilter.type = 'highpass'; // 低阶通过
+    biquadFilter.Q.value = 4;
+    biquadFilter.frequency.value = freq || 800; // 临界点的 Hz，默认800Hz
+    this.gain.connect(biquadFilter);
+    biquadFilter.connect(this.analyser);
+    this.analyser.connect(this.context.destination);
+  }
+  /*
+  * 人声增益 -- perfect --
+  */
+  public enhanceVocal() {
+    this.disconnect();
+    const gain1 = this.context.createGain();
+    const gain2 = this.context.createGain();
+    const channelSplitter = this.context.createChannelSplitter(2);
+    const channelMerger = this.context.createChannelMerger(2);
+
+    gain1.gain.value = 2;
+    gain2.gain.value = 2;
+
+    this.gain.connect(channelSplitter);
+
+    channelSplitter.connect(gain1, 0);
+    gain1.connect(channelMerger, 0, 1);
+    channelSplitter.connect(channelMerger, 1, 1);
+
+    channelSplitter.connect(gain2, 1);
+    gain2.connect(channelMerger, 0, 0);
+    channelSplitter.connect(channelMerger, 0, 0);
+
+    channelMerger.connect(this.analyser);
+    this.analyser.connect(this.context.destination);
+  }
+
+  public removeVocal() {
+    this.disconnect();
+    const gain1 = this.context.createGain();
+    const gain2 = this.context.createGain();
+    const gain3 = this.context.createGain();
+    const channelSplitter = this.context.createChannelSplitter(2);
+    const channelMerger = this.context.createChannelMerger(2);
+
+    // 反相音频组合
+    gain1.gain.value = -1;
+    gain2.gain.value = -1;
+
+    this.gain.connect(this.analyser);
+    this.analyser.connect(channelSplitter);
+
+    // 交叉音轨，减去相同的音频部分（即人声）
+    channelSplitter.connect(gain1, 0);
+    gain1.connect(channelMerger, 0, 1);
+    channelSplitter.connect(channelMerger, 1, 1);
+
+    channelSplitter.connect(gain2, 1);
+    gain2.connect(channelMerger, 0, 0);
+    channelSplitter.connect(channelMerger, 0, 0);
+
+    channelMerger.connect(gain3);
+    gain3.connect(this.context.destination);
+  }
+  /*
+  * 延时，回声效果
+  */
+  public delay() {
+    this.disconnect();
+
+    const delay = this.context.createDelay();
+    const gain1 = this.context.createGain();
+
+    delay.delayTime.value = 0.06; // 延时0.06s
+    gain1.gain.value = 1.2;
+
+    // 两条平行通路
+
+    // 1.source -> destination
+    this.gain.connect(this.context.destination);
+
+    // 2.source -> delay -> gain -> destination
+    this.gain.connect(delay);
+    delay.connect(gain1);
+    gain1.connect(this.analyser);
+    this.analyser.connect(this.context.destination);
+  }
+  /*
+       * 清除音效，还原原声
+       */
+  public cancelEffect() {
+    this.disconnect();
+    this.gain.connect(this.analyser);
+    this.analyser.connect(this.context.destination);
+  }
+  /*
+  *  左声道和右声道的分离并单独处理，最后合并，左耳低阶增强，右耳伴奏增强
+  */
+  public splitterMerger() {
+    this.disconnect();
+    const lGain = this.context.createGain();
+    const rGain = this.context.createGain();
+    // 声道离合器
+    const splitter = this.context.createChannelSplitter(2);
+    const merger = this.context.createChannelMerger(2);
+    lGain.gain.value = 1;
+    rGain.gain.value = 1;
+
+    const leftFilter = this.context.createBiquadFilter();
+    leftFilter.type = 'lowshelf';
+    // leftFilter.Q.value = 2;
+    leftFilter.gain.value = 10;
+    leftFilter.frequency.value = 392;
+
+    const rightFilter = this.context.createBiquadFilter();
+    rightFilter.type = 'highshelf';
+    // rightFilter.Q.value = 2;
+    rightFilter.gain.value = 10;
+    rightFilter.frequency.value = 82; // 临界点的 Hz，
+
+    this.gain.connect(splitter);
+    splitter.connect(lGain, 0);
+    splitter.connect(rGain, 1);
+    lGain.connect(leftFilter);
+    rGain.connect(rightFilter);
+    leftFilter.connect(merger, 0, 0);
+    rightFilter.connect(merger, 0, 1);
+    merger.connect(this.analyser);
+    this.analyser.connect(this.context.destination);
+  }
+
+
+
+
 }
+
 
 
 // class Music {
