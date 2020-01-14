33a6e2f43db4e0723e5d2ee84a40e6970fbca940
zh <only_tigerhu@163.com>
2020-01-14 18:00:29 +0800

    await

--- src/background.ts
+++ src/background.ts
@@ -14,7 +14,6 @@ import {
 } from '@/constant/ipc';
 import DockMenu from './constant/dock.menu';
 const isDevelopment = process.env.NODE_ENV !== 'production'
-
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win: BrowserWindow | any
