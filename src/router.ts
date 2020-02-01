2965154ac7f3ed9495ce02627a895ff023d859a9
zh <only_tigerhu@163.com>
2020-02-01 17:33:01 +0800

    解决冲突

--- src/router.ts
+++ src/router.ts
@@ -13,7 +13,7 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home,
-      redirect: '/find',
+      redirect: '/find/recommed',
       children: [
         ...Nav,
       ],
