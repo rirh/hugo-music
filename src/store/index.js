ad4fc7c4a49bc88e70872d545ccf98f3c389bf6c
TigerZH <only_tigerhu@163.com>
2021-11-17 19:48:48 +0800

    feat:add vuex persisted

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,9 @@
 import { createStore } from "vuex";
+import createPersistedState from "vuex-persistedstate";
+
 import sound from "@/views/home/_store";
 export default createStore({
+  plugins: [createPersistedState()],
   state: {},
   mutations: {},
   actions: {},
