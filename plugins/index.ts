33e9a6d60135ed39e8a6d3717cb9040e9d9bc78d
tigerzh <only_tigerhu@163.com>
2022-07-15 18:33:29 +0800

    feat(t): 添加版本和时间

--- plugins/index.ts
+++ plugins/index.ts
@@ -1,10 +1,11 @@
 import vue from '@vitejs/plugin-vue';
+import dayjs from 'dayjs'
 import createAutoImport from './auto-import'
 import createCompression from './compression'
 import createSvgIcon from './svg-icon'
 
 export default function createVitePlugins(viteEnv: any, isBuild = false) {
-    process.env.VITE_APP_BUILD_TIME = `${Date.now()}`;
+    process.env.VITE_APP_BUILD_TIME = `${dayjs().format("YYYY-MM-DD HH:mm:ss")}`; ;
     const vitePlugins = [vue()]
     vitePlugins.push(createAutoImport())
     vitePlugins.push(createSvgIcon(isBuild))
